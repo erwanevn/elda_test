d2405ebdbd0ac9415ca709f6046448a0bea7117a
erwanevn <contact@erwan-eveno.fr>
2025-11-02 17:15:14 +0100

    fix(backend): fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import routes from './routes/index.ts'
 
 dotenv.config()
@@ -8,6 +9,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // Middlewares
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 // Routes
